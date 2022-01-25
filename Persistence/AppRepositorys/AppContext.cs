b18aac669bd9b4a519688a34ea9170f86ab82233
Joan Andrés <joanandres318@gmail.com>
2022-01-25 15:59:41 -0500

    commit antes de implementar una nueva entidad de persona

--- Persistence/AppRepositorys/AppContext.cs
+++ Persistence/AppRepositorys/AppContext.cs
@@ -2,23 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using Domain;
 
 
-namespace Persistence
+namespace Persistence;
+
+public class AppContext : DbContext
 {
 
-    public class AppContext : DbContext
+    //Se crea la propiedad DbSet de la entidad Contact de la capa Domain que permite aplicar el CRUD sobre la DB
+    public DbSet<Contact> Contacts { get; set; }
+
+    // Se crea la función OnConfiguring que permite la conexión y configuración con la base de datos
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-
-        //Se crea la propiedad DbSet de la entidad Contact de la capa Domain que permite aplicar el CRUD sobre la DB
-        public DbSet<Contact> Contacts { get; set; }
-
-        // Se crea la función OnConfiguring que permite la conexión y configuración con la base de datos
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        if (!optionsBuilder.IsConfigured)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=SIAPprojectData");
-            }
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=SIAPprojectData");
         }
-
     }
+
 }
---
+++ Persistence/Migrations/20220125202238_FirtsMigrations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class FirtsMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
