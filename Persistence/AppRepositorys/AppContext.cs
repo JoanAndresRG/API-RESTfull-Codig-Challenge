ea4fd8b864f28b14b070f00fe324b42af0dc9b05
Joan Andrés <joanandres318@gmail.com>
2022-01-25 15:17:36 -0500

    arquitectura terminada

---
+++ Persistence/AppRepositorys/AppContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+
+namespace Persistence
+{
+
+    public class AppContext : DbContext
+    {
+
+        //Se crea la propiedad DbSet de la entidad Contact de la capa Domain que permite aplicar el CRUD sobre la DB
+        public DbSet<Contact> Contacts { get; set; }
+
+        // Se crea la función OnConfiguring que permite la conexión y configuración con la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=SIAPprojectData");
+            }
+        }
+
+    }
+}
